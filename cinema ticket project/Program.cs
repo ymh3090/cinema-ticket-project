b6891eb6d8a3a51249b18048f3e686b66fb664f9
Yahia Hamed <yahiamh309090@outlook.com>
2025-08-19 14:36:24 +0300

    ..

--- cinema ticket project/Program.cs
+++ cinema ticket project/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("| Cinema Ticket Booking System ");
+
+            Console.Write("Enter username: ");
+            string? username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string? password = Console.ReadLine();
         }
     }
 }
